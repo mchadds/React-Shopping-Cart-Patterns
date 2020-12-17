9584de1385ef5911315e57c977e1d0efc4f239a0
mchadds <cdundassmith@gmail.com>
2020-12-16 19:34:14 -0700

    pass counter instead of  properties to component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
   };
 
   product = {
@@ -29,6 +29,7 @@ class Counter extends Component {
         >
           Increment
         </button>
+        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
       </div>
     );
   }
